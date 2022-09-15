77761d294280bb2487a98a5811b392965259cff9
InvasorDaveM <invasor.davem@gmail.com>
2022-09-14 23:59:55 -0500

    Renombramiento de paquetes

--- src/main/java/com/project/sheikah_store/controller/controller.java
+++ src/main/java/com/project/sheikah_store/controller/controller.java
@@ -1,4 +1,4 @@
-package com.project.sheika_store.controller;
+package com.project.sheikah_store.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
