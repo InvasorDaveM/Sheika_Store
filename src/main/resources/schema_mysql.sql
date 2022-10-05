a9b90a92783f0d25afc9c4e50707678e0f3b1ead
InvasorDaveM <invasor.davem@gmail.com>
2022-10-05 17:08:18 -0500

    INSERT de GoW

--- src/main/resources/schema_mysql.sql
+++ src/main/resources/schema_mysql.sql
@@ -4,7 +4,7 @@ CREATE TABLE game(
     category VARCHAR(50) NOT NULL,
     price DOUBLE NOT NULL,
     description_game TEXT NOT NULL,
-    image_path VARCHAR(100) NOT NULL,
+    image_path VARCHAR(50) NOT NULL,
     producer VARCHAR(50) NOT NULL
 );
 
@@ -17,59 +17,16 @@ INSERT INTO game(
     producer
 )
 VALUES(
-    "Kirbo",
-    "Pipi",
-    60.00,
-    "Kirbo mata a gente",
-    "https://d3u63wyfuci0ch.cloudfront.net/wp-content/uploads/2017/07/05174055/tumblr_ort0kfbk9t1s43f3wo1_500.png",
-    "Nintendon't"
-),(
-    "Zelda",
-    "Popo",
-    65.00,
-    "Link osuno",
-    "https://d3u63wyfuci0ch.cloudfront.net/wp-content/uploads/2017/07/05174242/tumblr_orfzm2LkyE1s43f3wo1_500.png",
-    "Nintendon't"
-),(
-    "Mario",
-    "Pepe",
-    80.00,
-    "Mario osito ricolino",
-    "https://d3u63wyfuci0ch.cloudfront.net/wp-content/uploads/2017/07/05173944/tumblr_or3k7qhsoF1s43f3wo1_500.png",
-    "Nintendon't"
-),(
-    "Luigo",
-    "Papa",
-    40.00,
-    "Mario verde osito ricolino",
-    "https://d3u63wyfuci0ch.cloudfront.net/wp-content/uploads/2017/07/05174300/tumblr_orjqwmDPjP1s43f3wo1_500-1.png",
-    "Nintendon't"
-),(
-    "DK",
-    "Pupu",
-    30.00,
-    "Furro ricolino",
-    "https://d3u63wyfuci0ch.cloudfront.net/wp-content/uploads/2017/07/05174020/tumblr_orahi14rP11s43f3wo1_500.png",
-    "Nintendon't"
-),(
-    "Fost",
-    "Papi",
-    20.00,
-    "Furro espacial ricolino",
-    "https://d3u63wyfuci0ch.cloudfront.net/wp-content/uploads/2017/07/05174130/tumblr_os0iai8p2W1s43f3wo1_500.png",
-    "Nintendon't"
-),(
-    "Yigli",
-    "Popu",
-    80.00,
-    "Globo asexual",
-    "https://d3u63wyfuci0ch.cloudfront.net/wp-content/uploads/2017/07/05174224/tumblr_osde8sRnN21s43f3wo1_500-1.png",
-    "Nintendon't"
-),(
-    "Captain Falcon",
-    "Pipe",
-    80.00,
-    "Man menos rico que Snake pero igual rico",
-    "https://d3u63wyfuci0ch.cloudfront.net/wp-content/uploads/2017/07/05174036/tumblr_orninkB0WW1s43f3wo1_500.png",
-    "Nintendon't"
-);
+    "Gof of War",
+    "Hack 'n' Slash",
+    19.99,
+    "Kratos is a father again. As mentor and protector to Atreus, a son determined to earn his respect, he is forced to deal with and control the rage that has long defined him while out in a very dangerous world with his son.
+
+His vengeance against the Gods of Olympus behind him, Kratos now lives in the realm of Norse deities and monsters.
+
+It’s in this harsh, unforgiving world that he must fight to survive, and not only teach his son to do the same… but also prevent him from repeating the Ghost of Sparta’s bloodstained mistakes.
+
+This staggering reimagining of God of War combines all the hallmarks of the iconic series – brutal combat, epic boss fights, and breathtaking scale – and fuses them with a powerful and moving narrative that re-establishes Kratos’ world. ",
+    "img/products/God of War.png",
+    "Sony Interactive Entertainment"
+)
