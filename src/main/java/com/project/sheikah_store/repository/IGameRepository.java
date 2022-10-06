a9a36a8d495ef9afb6d9caf5bb9a110ea8a879f7
InvasorDaveM <invasor.davem@gmail.com>
2022-10-06 06:25:27 -0500

    Agregación de algunos comentarios guía

--- src/main/java/com/project/sheikah_store/repository/IGameRepository.java
+++ src/main/java/com/project/sheikah_store/repository/IGameRepository.java
@@ -9,7 +9,7 @@ import com.project.sheikah_store.model.Game;
 
 public interface IGameRepository extends JpaRepository<Game, Integer> {
 
-    @Query("FROM Game")
+    @Query("FROM Game") // Se pone el nombre de la clase en proyecto Java, no el de la entidad de la BD
     // @Query(value = "SELECT * FROM Game", nativeQuery = true)
     public List<Game> findAllGames();
 
